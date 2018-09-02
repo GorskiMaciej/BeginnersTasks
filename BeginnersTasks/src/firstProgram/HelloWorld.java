39a8c866531a75bfc78e44b34cc904b2f4b16ac7
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2018-09-02 21:05:25 +0200

    Adding class to shout in main

--- BeginnersTasks/src/firstProgram/HelloWorld.java
+++ BeginnersTasks/src/firstProgram/HelloWorld.java
@@ -3,8 +3,7 @@ package firstProgram;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("After second commit.");
+		Shouting.Shout();
 
 	}
 
---
+++ BeginnersTasks/src/firstProgram/Shouting.java
@@ -0,0 +1,9 @@
+package firstProgram;
+
+public class Shouting {
+	
+	public static void Shout(){
+		System.out.println("Hello world!");
+	}
+
+}
